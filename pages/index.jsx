6798623efd7a86daa4380e88191e05d98429d18a
daiju81 <1d07c.m8k.prv@gmail.com>
2020-06-20 17:12:20 +0900

    withFormik

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,75 +1,63 @@
 import React, { Component } from 'react';
 import Router from 'next/router';
 import {
-  Formik,
-  Form,
-  Field,
-  ErrorMessage
+  withFormik,
+  Formik
 } from 'formik';
 import * as Yup from 'yup';
 
-const validationSchema = Yup.object().shape({
-  email: Yup.string()
-    .email("メールアドレスの形式に誤りがあります")
-    .required("メールアドレスは必須です"),
-  password: Yup.string().required("パスワード設定は必須です"),
-  confirmPassword: Yup.string()
-    .required("設定したパスワードを再入力してください")
-    .oneOf([Yup.ref("password")], "パスワードが一致しません"),
-});
-class RegistrationForm extends Component {
-  constructor(props) {
-    super(props);
-    this.defaultFormState = {
-      email: "",
-      password: "",
-      confirmPassword: "",
-    };
-  }
-  /**
-   * フォーム送信後の処理
-   */
-  handleSubmit(form) {
-    // 値をコンソール表示
-    console.log(form);
-  }
-  render() {
-    return (
-      <Formik
-        onSubmit={this.handleSubmit}
-        initialValues={this.defaultFormState}
-        validationSchema={validationSchema}
-      >
-        <Form>
-          <Field name="email" type="email" placeholder="Email" />
-          <Field name="password" type="password" placeholder="Password" />
-          <Field
-            name="confirmPassword"
-            type="password"
-            placeholder="Confirm password"
-          />
-          <button type="submit">Submit</button>
-          <ErrorMessage name="email" component="div" className="invalidForm" />
-          <ErrorMessage
-            name="password"
-            component="div"
-            className="invalidForm"
-          />
-          <ErrorMessage
-            name="confirmPassword"
-            component="div"
-            className="invalidForm"
-          />
-        </Form>
-      </Formik>
-    );
-  }
-}
+const MyForm = (props) => {
+  const {
+    values,
+    touched,
+    errors,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+  } = props;
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        value={values.name}
+        name="name"
+      />
+      {errors.name && touched.name && <div id="feedback">{errors.name}</div>}
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+const MyEnhancedForm = withFormik({
+  mapPropsToValues: () => ({ name: "" }),
+
+  // Custom sync validation
+  validate: (values) => {
+    const errors = {};
+
+    if (!values.name) {
+      errors.name = "Required";
+    }
+
+    return errors;
+  },
+
+  handleSubmit: (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 1000);
+  },
+
+  displayName: "BasicForm",
+})(MyForm);
 
 const Index = () => (
   <div>
     <p>Hello</p>
-    <RegistrationForm />
+    <MyEnhancedForm />
   </div>
 );
 
