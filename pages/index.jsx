c246bcb4dc365b653389e27ead3c4aa6ed78f883
daiju81 <1d07c.m8k.prv@gmail.com>
2020-06-28 12:47:01 +0900

    fixed

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,69 +1,63 @@
-import React, { Component } from 'react';
-import Router from 'next/router';
-import {
-  withFormik,
-  Formik
-} from 'formik';
-import * as yup from 'yup';
+import React from "react";
+import { Formik } from "formik";
 
-const MyForm = (props) => {
-  const {
-    values,
-    touched,
-    errors,
-    handleChange,
-    handleBlur,
-    handleSubmit,
-  } = props;
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values.name}
-        name="name"
-      />
-      {errors.name && <div className="errorText">{errors.name}</div>}
-      <input
-        type="email"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values.email}
-        name="email"
-      />
-      {errors.email && touched.email && <div id="feedback">{errors.email}</div>}
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
-
-const MyEnhancedForm = withFormik({
-  mapPropsToValues: () => ({ name: "", email: "" }),
-
-  
-
-  // Custom sync validation
-  validationSchema: yup.object().shape({
-    name: yup.string().required(),
-    email: yup.string().email().required(),
-  }),
-
-  handleSubmit: (values, { setSubmitting }) => {
-    setTimeout(() => {
-      alert(JSON.stringify(values, null, 2));
-      setSubmitting(false);
-    }, 1000);
-  },
-
-  displayName: "BasicForm",
-})(MyForm);
-
-const Index = () => (
+const Basic = () => (
   <div>
-    <p>Hello</p>
-    <MyEnhancedForm />
+    <h1>Anywhere in your app!</h1>
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validate={(values) => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = "Required";
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = "Invalid email address";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+          />
+          {errors.email && touched.email && errors.email}
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.password}
+          />
+          {errors.password && touched.password && errors.password}
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
   </div>
 );
 
-export default Index;
+export default Basic;
